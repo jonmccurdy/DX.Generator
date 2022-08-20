f9b757b9f48b9ce9499e98e3d2c737d088ca3a44
Jon McCurdy <jonmccurdy@ymail.com>
2022-08-20 15:49:50 -0500

    Initial Commit

---
+++ R/dx_init.R
@@ -0,0 +1,39 @@
+#' @title dx_init
+#' @name dx_init
+#'
+#' @description 
+#' \code{dx_init()} allows the user to designate the DX-class of Pseudo-Random Number Generators 
+#' as the user-supplied RNG in R. This makes it so where \code{runif()} and any other function 
+#' relying on \code{runif()} will use a DX-k-s RNG to generate the random variate. 
+#' 
+#' @details 
+#' Within this function there are two arguments which can be passed through the function.
+#' \eqn{K} determines the order of the linear occurrence, and \eqn{S} determines the number of non-zero 
+#' coefficients in the MRG. 
+#' 
+#' Speed is the fastest for the default values of \eqn{S}, that is \eqn{S = 1}.  
+#' The argument \eqn{K} has no affect on the speed of the generator. The argument \eqn{S} essentially 
+#' controls how many additions are performed to produce the next random number: the fewer, the faster.
+#' 
+#' Although DX-\eqn{k}-\eqn{s} generators have excellent empirical properties and can pass the most 
+#' stringent battery of tests for uniformity, those set with \eqn{s = 4} will appear marginally more 
+#' randomly and uniformly distributed. 
+#' 
+#' Often times in simulations, a consecutive run of random numbers need to appear independent with 
+#' small serial correlations between any successive subset of numbers.  If \eqn{j} consecutive 
+#' numbers need to appear independent, then choose argument \eqn{K} such that \eqn{K > j}.
+#' 
+#' @param K the order of the linear recurrence. \eqn{K} must be an integer between 5 and 3500
+#' @param S number of non-zero coefficients in the MRG. \eqn{S} must be an either 1,2,3 or 4
+#'
+#' @examples
+#' \dontrun{dx_init(K=53, S=2)}
+#' 
+#' @references 
+#' L.-Y. Deng and H. Xu. A system of high-dimensional, efficient, long-cycle and portable uniform random number generators. ACM Transactions on Modeling and Computer Simulation (TOMACS), 13 (4):299–309, 2003.
+
+dx_init <- function(K=47, S=1) {
+  invisible(capture.output(setRNG::setRNG(kind="user-supplied", seed=c(K,S,.Random.seed[4])),type = "output"))
+  invisible(runif(1))
+  set.seed(.Random.seed[4])
+}
---
+++ src/RNG-c-code.cpp
@@ -0,0 +1,149 @@
+#include <Rcpp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <R_ext/Random.h>
+using namespace Rcpp;
+
+#define K_MAX	3500
+#define PP 2147483647  //2^31-1
+#define IPP 4.6566129e-10
+#define I32 2.328306437e-10;
+#define ROTR(x,n) ((((unsigned long) x)>>(n))|(((unsigned long) x)<<(32-(n))))
+
+#define TSIZE 256 		// actual table size used
+#define maxT 1024 		//maximum table size
+#define maxB 15
+#define B_X 32896  		
+#define B_Y 32776
+#define T_MOD TSIZE-1
+#define MOD2_32 4294967295  
+
+static long long B_X1 = 536869888; // will change based on K, S- can be done more efficiently
+static long long B_X2 = 65011712;
+static long long B_X3 = 67633152;
+static long long B_X4 = 67108736;
+
+static int I_X;           /* running index */
+
+unsigned long MODP(unsigned long z) {return ((((z)&PP)+((z)>>31)) &PP);}
+
+static int K_X = 47;
+static int S_X = 1;
+static int nseed = 3;
+static int seed[K_MAX]={K_X,S_X};
+static int XX[K_MAX];
+static double res;
+int set=0;
+
+
+
+
+int i, K12, K13, K23;
+
+void dx_1(){
+  K_X = seed[0];
+  S_X = seed[1];
+  int II0 = I_X;
+  if(++I_X >= K_X)  I_X = 0;     /*wrap around running index */
+  XX[I_X] = MODP(B_X1 * XX[I_X] + XX[II0]);
+  res = (double) XX[I_X] * IPP;
+}
+
+void dx_2(){
+  K_X = seed[0];
+  S_X = seed[1];
+  int II0 = I_X;
+  if(++I_X >= K_X)  I_X = 0;     /*wrap around running index */
+  XX[I_X] = MODP(B_X2 * (XX[I_X] + XX[II0]));
+  res = (double) XX[I_X] * IPP;
+}
+
+void dx_3(){
+  K_X = seed[0];
+  S_X = seed[1];
+  int II0 = I_X;
+  if(++I_X >= K_X)  I_X = 0;     /*wrap around running index */
+  if(++K12 >= K_X) K12 = 0;    /*wrap around K12*/
+  XX[I_X] = MODP(B_X3 * (XX[I_X] + XX[K12] + XX[II0]));
+  res= (double) XX[I_X] * IPP;
+}
+
+void dx_4(){
+  K_X = seed[0];
+  S_X = seed[1];
+  int II0 = I_X;
+  if(++I_X >= K_X)   I_X = 0;    /*wrap around running index */
+  if(++K13 >= K_X) K13 = 0;    /*wrap around K13*/
+  if(++K23 >= K_X) K23 = 0;    /*wrap around K23*/
+  XX[I_X] = MODP(B_X4 * (XX[I_X]+XX[K13]+XX[K23]+XX[II0]));
+  res= (double) XX[I_X] * IPP;
+}
+
+void dx_init(){
+  for (i=0; i< K_MAX; i++){
+    XX[i] = R::runif(0,PP);
+  }
+}
+
+void (*dx_gen)();
+
+void generator_type(){
+  switch(S_X){
+  case 1: dx_gen=&dx_1;
+    dx_init();
+    break;
+  case 2: dx_gen=&dx_2;
+    dx_init();
+    break;
+  case 3: dx_gen=&dx_3;
+    dx_init();
+    break;
+  case 4: dx_gen=&dx_4;
+    dx_init();
+    break;
+  default: dx_gen=&dx_1;
+  dx_init();
+  S_X=1;
+  seed[1]=S_X;
+  Rcerr << "The value of 'S' that was chosen is not compatible with this package. \nBy default, a DX-" << K_X<< "-1 generator was chosen.\n";
+  break;
+  }
+}
+
+double * user_unif_rand(){
+  dx_gen();
+  return &res;
+}
+
+void user_unif_init(Int32 seed_in) {
+  K_X = seed[0];
+  S_X = seed[1];
+  
+  seed[2] = R::runif(0,PP);
+  
+  if(K_X<5){
+    K_X=47;
+    seed[0]=K_X;
+    Rcerr << "The value of 'K' that was chosen is not compatible with this package. \nBy default, a DX-47-" << S_X<< " generator was chosen.\n";
+  }
+  
+  if(K_X>3500){
+    K_X=47;
+    seed[0]=K_X;
+    Rcerr << "The value of 'K' that was chosen is not compatible with this package. \nBy default, a DX-47-" << S_X<< " generator was chosen.\n";
+  }
+  generator_type();
+  
+  I_X = seed[0]-1;
+  K12 = seed[0]/2-1;
+  K13 = 2*seed[0]/3-1;   
+  K23 = seed[0]/3-1;
+  
+  if(set!=0){
+    Rcout << "# You are currently using a DX-" << K_X << "-" << S_X << " Generator.\n# To alter it use the dx_init() function.\n";
+  }
+  set=1;
+}
+
+int * user_unif_nseed() {return &nseed; }
+int * user_unif_seedloc() { return (int *) &seed; }
